1c560816ea4129e9ec8f7cd3299425adb47d302c
Ben Gould <ben@bengould.co.uk>
2016-01-25 23:27:35 +0000

    Fixed failing tests

--- Tests/Filter-TwitterResults.Tests.ps1
+++ Tests/Filter-TwitterResults.Tests.ps1
@@ -2,6 +2,9 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\..\$sut"
 
+. "$here\..\New-TwitterAuthorisation.ps1"
+. "$here\..\Get-TwitterTimeline.ps1"
+
 Describe "Filter-TwitterResults" {
     It "returns a match" {
        Filter-TwitterResults -Tweeter kzdrfa -Hashtag "chif" | Should not be $null
--- Tests/Get-TwitterTimeline.Tests.ps1
+++ Tests/Get-TwitterTimeline.Tests.ps1
@@ -1,6 +1,8 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\..\$sut"
+. "$here\..\New-TwitterAuthorisation.ps1"
+
 
 Describe "Get-TwitterTimeline" {
     It "should send a good request" {
